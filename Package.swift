5e4b8c39416ca68b64fdc2d245036c85dd6868d8
Ken Pham <klpham1016@gmail.com>
2022-02-28 14:51:08 -0500

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
-    name: "Pantomime"
+    name: "Pantomime",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(name: "Pantomime", targets: [ "Pantomime" ]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "Pantomime", dependencies: [], path: "Sources"),
+    ]
 )
